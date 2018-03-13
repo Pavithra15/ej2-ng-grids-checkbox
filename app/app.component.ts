9c7a11e6a23539d89a8723f56693c38c2504663f
Pavithra15 <34119270+Pavithra15@users.noreply.github.com>
2018-03-13 17:54:02 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { closest  } from '@syncfusion/ej2-base';
+import { GridComponent } from '@syncfusion/ej2-ng-grids';
+import { orderData } from './data';
+
+@Component({
+    selector: 'my-app',
+    template: `<ejs-grid #grid [dataSource]='data' [toolbar]='toolbar' id='grid' (click)='recordClick($event)' [editSettings]='edit' allowPaging=true allowSelection=true>
+                  <e-columns>
+                  <e-column field='OrderID' headerText='ID' isPrimaryKey=true width='120' textAlign='Right'></e-column>
+                  <e-column field='CustomerID' headerText='Name' width='160'></e-column>
+                   <e-column field='Active' headerText='' type='checkbox' width='160'></e-column>
+                  </e-columns>
+                </ejs-grid>`
+})
+export class AppComponent implements OnInit {
+
+    public data: Object[];
+    public edit: Object;
+    public toolbar: string[];
+    public selection: Object;
+    public page: Object;
+
+    @ViewChild('grid')
+    public grid: GridComponent;
+    ngOnInit(): void {
+        this.data = [{ OrderID: 10248, CustomerID: 'VINET', Active: false },
+        { OrderID: 10249, CustomerID: 'TOMSP', Active: true },
+        { OrderID: 10250, CustomerID: 'HANAR', Active: false }];
+        this.edit = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch', showConfirmDialog: true };
+        this.toolbar = ['Add', 'Edit', 'Delete', 'Cencel', 'Update'];
+
+
+    }
+    recordClick(e: any) {
+        if (e.target.classList.contains('e-rowcell')) {
+            let cell: HTMLTableCellElement = <HTMLTableCellElement>closest(<Element>e.target, 'td');
+            let tr: HTMLTableRowElement = <HTMLTableRowElement>closest(cell, '.e-row');
+            if (tr) {
+                let val: boolean = tr.getAttribute('aria-selected') ? false : true;
+                let rowIndex: number = parseInt(cell.parentElement.getAttribute('aria-rowindex'), 10);
+                this.grid.editModule.updateCell(rowIndex, 'Active', val);
+                this.grid.editModule.batchSave();
+                let btn: any = document.querySelector('#gridEditConfirm').children[1].children[0];
+                btn.click();
+            }
+        }
+    }
+}
